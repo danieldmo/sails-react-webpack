f4602120e12473183a0971c2f33d3143cdab61ce
Daniel <daniel@boxfish.com>
2017-03-30 14:32:43 -0700

    cv fix

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -1,14 +1,12 @@
 module.exports = {
   index: function (req, res) {
 
-    return res.view('cv');
+    var bundle;
 
-    // var bundle;
+    if (sails.config.environment !== 'development') {
+      bundle = require('../../assets.json').main.js;
+    }
 
-    // if (sails.config.environment !== 'development') {
-    //   bundle = require('../../assets.json').main.js;
-    // }
-
-    // return res.view('index', { bundle });
+    return res.view('index', { bundle });
   },
 };
